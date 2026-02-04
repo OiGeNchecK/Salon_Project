b824647816249c8b095439afd5c4192330bc70c0
OiGeN <111658379+OiGeN-DyuK@users.noreply.github.com>
2026-02-04 20:07:46 +0100

    homework_69

--- src/main/java/org/example/salon_project/controller/PingController.java
+++ src/main/java/org/example/salon_project/controller/PingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class pingController {
+public class PingController {
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
